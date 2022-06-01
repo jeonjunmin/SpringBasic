c1f85257c34d8bb8cb61a1459b34ed533767a831
jeonjunmin <azeros314@naver.com>
2022-06-01 22:40:50 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기 / 탐색 위치와 기본 스캔 대상

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+
+@Configuration
+@ComponentScan(
+        //컴포넌트 스캔은 이름 그대로 @Component 어노테이션이 붙은 클래스를 스캔해 스프링빈에 등록한다.
+        //컴포넌트 스캔을 사용하면 @Configuration이 붙은 설정 정보도 자동으로 빈컨테이너에 등록되기 때문에 excludeFilters를 이용해서 Configuration을 컴포넌트 스캔대상에서 제외한다.
+        excludeFilters = @ComponentScan.Filter(type =  FilterType.ANNOTATION, classes = Configuration.class)
+        //빈컨테이너에 등록할 컴포넌트의 스캔 시작점을 설정할 수 있다. 기본은 Appconfig파일 위치 기준패키지의 하위를 다 뒤진다.
+//        ,basePackages = "hello.core.member"
+
+)
+public class AutoAppConfig {
+
+
+}
--- src/main/java/hello/core/order/OderServiceImpl.java
+++ src/main/java/hello/core/order/OderServiceImpl.java
@@ -3,7 +3,10 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OderServiceImpl implements OrderService{
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -13,6 +16,7 @@ public class OderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired //생성자에 자동으로 의존관계를 주입시켜준다.
     public OderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
