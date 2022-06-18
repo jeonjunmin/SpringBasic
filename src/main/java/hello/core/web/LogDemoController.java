9678159b3995a4d476c4897822b25ca1085a2f50
jeonjunmin <azeros314@naver.com>
2022-06-18 17:33:13 +0900

    request 스코프 예제 만들기

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor //생성자에 Autowire를 자동으로 설정해준다.
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestUrl = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestUrl);
+
+        myLogger.log("controller Test");
+        logDemoService.logic("testID");
+
+        return "OK!!";
+    }
+}
